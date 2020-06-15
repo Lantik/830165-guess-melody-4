fd3d193b0ab7f44c772349dee9e8fafb9e955c56
Lantik <vladyslav.svietlov@ukad-group.com>
2020-06-15 12:34:00 +0300

    [FEATURE]: added app and welcome screen tests

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
+const onWelcomeButtonClickHandler = () => {};
+
 const App = ({errorsCount}) => {
   return (<React.Fragment>
-    <WelcomeScreen errorsCount={errorsCount}></WelcomeScreen>
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={onWelcomeButtonClickHandler}>
+    </WelcomeScreen>
   </React.Fragment>);
 };
 
